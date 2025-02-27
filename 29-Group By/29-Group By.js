226d5e11ab006d0cda1ab2536d9a56bb287bd8bf
parsaempir <parsa.aysham54@gmail.com>
2025-02-27 05:07:07 +0330

    new

---
+++ 29-Group By/29-Group By.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        (acc[key] ||= []).push(item);
+        return acc;
+    }, {}) 
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
